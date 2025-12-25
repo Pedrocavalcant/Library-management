953b79ee50aae8b2ab0a05852187223373f58796
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-12-25 19:28:38 -0300

    Add project files.

---
+++ Library Management Api/Controllers/BooksController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management_Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class BooksController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+}
