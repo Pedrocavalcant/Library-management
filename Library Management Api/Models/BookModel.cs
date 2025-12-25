9b06103487847c9bc4b20da13c56b34b1339640c
Pedrocavalcant <pedrocavalcante50256@gmail.com>
2025-12-25 19:43:28 -0300

    Feat, model de livro

---
+++ Library Management Api/Models/BookModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Management_Api.Models;
+
+public class BookModel
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Genre { get; set; }
+    public double Price { get; set; }
+    public int Stock { get; set; }
+}
